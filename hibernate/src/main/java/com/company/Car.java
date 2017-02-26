9c0da35499b247ce2672728c176ce8be5a82912e
egglestt <egglestt@mail.gvsu.edu>
2017-02-25 20:10:18 -0500

    Installed JDBC, Maven, and Hibernate. Created Car.java, DBaccess.java, and car.hbm.xml

---
+++ hibernate/src/main/java/com/company/Car.java
@@ -0,0 +1,116 @@
+package com.company;
+//package com.javatpoint.mypackage;
+
+/**
+ * Created by Timothy on 2/25/2017.
+ */
+public class Car {
+    private int id, cyl, disp, hp, gear, carb;
+    private String name;
+    private double mpg, drat, wt, qsec;
+    private boolean vs, am;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCyl() {
+        return cyl;
+    }
+
+    public void setCyl(int cyl) {
+        this.cyl = cyl;
+    }
+
+    public int getDisp() {
+        return disp;
+    }
+
+    public void setDisp(int disp) {
+        this.disp = disp;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getGear() {
+        return gear;
+    }
+
+    public void setGear(int gear) {
+        this.gear = gear;
+    }
+
+    public int getCarb() {
+        return carb;
+    }
+
+    public void setCarb(int carb) {
+        this.carb = carb;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getMpg() {
+        return mpg;
+    }
+
+    public void setMpg(double mpg) {
+        this.mpg = mpg;
+    }
+
+    public double getDrat() {
+        return drat;
+    }
+
+    public void setDrat(double drat) {
+        this.drat = drat;
+    }
+
+    public double getWt() {
+        return wt;
+    }
+
+    public void setWt(double wt) {
+        this.wt = wt;
+    }
+
+    public double getQsec() {
+        return qsec;
+    }
+
+    public void setQsec(double qsec) {
+        this.qsec = qsec;
+    }
+
+    public boolean isVs() {
+        return vs;
+    }
+
+    public void setVs(boolean vs) {
+        this.vs = vs;
+    }
+
+    public boolean isAm() {
+        return am;
+    }
+
+    public void setAm(boolean am) {
+        this.am = am;
+    }
+}
---
+++ hibernate/src/main/java/com/company/DBaccess.java
@@ -0,0 +1,36 @@
+package com.company;
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+
+/**
+ * Created by Timothy on 2/25/2017.
+ */
+public class DBaccess {
+    MysqlDataSource dataSource;
+
+    public static boolean DBaccess(){
+        try {
+            MysqlDataSource dataSource = new MysqlDataSource();
+            dataSource.setUser("veldhuit@orcl");
+            dataSource.setPassword("hibernate23");
+            dataSource.setServerName("dbserv.cis.gvsu.edu");
+
+            Connection conn = dataSource.getConnection();
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SHOW TABLES");
+            System.out.println(rs);
+
+            rs.close();
+            stmt.close();
+            conn.close();
+        }
+        catch (Exception ex) {
+            return false;
+        }
+        return true;
+    }
+}
